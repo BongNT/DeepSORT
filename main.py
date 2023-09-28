33b6dc7d87a91f1c153aaabddbbeb929a2eb997d
Bong <nguyenbong2008@gmail.com>
2023-09-28 09:45:49 +0700

    process in MOT data (img)

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class VideoTracker(object):
         self.device = select_device(args.device)
         self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
 
-        # create video capture ****************
+        # *****************************  create video capture ****************
         if args.display:
             cv2.namedWindow("test", cv2.WINDOW_NORMAL)
             cv2.resizeWindow("test", args.display_width, args.display_height)
@@ -78,12 +78,12 @@ class VideoTracker(object):
 
         # ************************* Load video from file *************************
         else:
-            assert os.path.isfile(self.args.input_path), "Path error"
-            self.vdo.open(self.args.input_path)
+            assert os.path.isfile(self.args.input_video), "Path error"
+            self.vdo.open(self.args.input_video)
             self.im_width = int(self.vdo.get(cv2.CAP_PROP_FRAME_WIDTH))
             self.im_height = int(self.vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
             assert self.vdo.isOpened()
-            print('Done. Load video file ', self.args.input_path)
+            print('Done. Load video file ', self.args.input_video)
 
         # ************************* create output *************************
         if self.args.save_path:
@@ -224,16 +224,284 @@ class VideoTracker(object):
         return outputs, t2-t1, t3-t2
 
 
+class ImageTracker(object):
+    def __init__(self, args):
+        self.args = args
+
+        self.img_size = args.img_size                   # image size in detector, default is 640
+        self.frame_interval = args.frame_interval       # frequency
+
+        self.device = select_device(args.device)
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        # *****************************  create video capture ****************
+        if args.display:
+            cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+            cv2.resizeWindow("test", args.display_width, args.display_height)
+
+        self.info = self.__gather_sequence_info(args.seq_folder)
+        # ***************************** initialize DeepSORT **********************************
+        cfg = get_config()
+        cfg.merge_from_file(args.config_deepsort)
+
+        use_cuda = self.device.type != 'cpu' and torch.cuda.is_available()
+        self.deepsort = build_tracker(cfg, use_cuda=use_cuda)
+
+        # ***************************** initialize YOLO-V5 **********************************
+        self.detector = torch.load(args.weights, map_location=self.device)['model'].float()  # load to FP32
+        self.detector.to(self.device).eval()
+        if self.half:
+            self.detector.half()  # to FP16
+
+        self.names = self.detector.module.names if hasattr(self.detector, 'module') else self.detector.names
+
+        print('Done..')
+        if self.device == 'cpu':
+            warnings.warn("Running in cpu mode which maybe very slow!", UserWarning)
+    
+    def __enter__(self):
+        # ************************* Load info from sequences *************************
+        self.im_height,self.im_width = self.info["image_size"][:2]
+        print('Done. Load info seq ', self.args.seq_folder)
+        # ************************* create output *************************
+        if self.args.save_path:
+            os.makedirs(self.args.save_path, exist_ok=True)
+            # path of saved video and results
+            self.save_video_path = os.path.join(self.args.save_path, f"results_{self.info['sequence_name']}.mp4")
+            # create video writer
+            fourcc = cv2.VideoWriter_fourcc(*self.args.fourcc)
+            self.writer = cv2.VideoWriter(self.save_video_path, fourcc,self.info["frameRate"], (self.im_width, self.im_height))
+            print('Done. Create output file ', self.save_video_path)
+        if self.args.save_result:
+            os.makedirs(self.args.save_result, exist_ok=True)
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.writer.release()
+        if exc_type:
+            print(exc_type, exc_value, exc_traceback)
+
+    def run(self):
+        yolo_time, sort_time, avg_fps = [], [], []
+        t_start = time.time()
+
+        idx_frame = 0
+        last_out = None
+        img_paths = self.info["image_filenames"]
+        for img_path in img_paths.values():
+            # Inference *********************************************************************
+            t0 = time.time()
+            print((img_path))
+            img0 = self.__read_img(img_path)
+
+            if idx_frame % self.args.frame_interval == 0:
+                outputs, yt, st = self.image_track(img0)        # (#ID, 5) x1,y1,x2,y2,id
+                last_out = outputs
+                yolo_time.append(yt)
+                sort_time.append(st)
+                print('Frame %d Done. YOLO-time:(%.3fs) SORT-time:(%.3fs)' % (idx_frame, yt, st))
+            else:
+                outputs = last_out  # directly use prediction in last frames
+            t1 = time.time()
+            avg_fps.append(t1 - t0)
+
+            # post-processing ***************************************************************
+            # visualize bbox  ********************************
+            if len(outputs) > 0:
+                bbox_xyxy = outputs[:, :4]
+                identities = outputs[:, -1]
+                img0 = draw_boxes(img0, bbox_xyxy, identities)  # BGR
+
+                # add FPS information on output video
+                text_scale = max(1, img0.shape[1] // 1600)
+                cv2.putText(img0, 'frame: %d fps: %.2f ' % (idx_frame, len(avg_fps) / sum(avg_fps)),
+                        (20, 20 + text_scale), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 0, 255), thickness=2)
+
+            # display on window ******************************
+            if self.args.display:
+                cv2.imshow("test", img0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    cv2.destroyAllWindows()
+                    break
+
+            # save to video file *****************************
+            if self.args.save_path:
+                self.writer.write(img0)
+
+            if self.args.save_result:
+                with open(self.args.save_txt + self.info["sequence_name"] + '.txt', 'a+') as f:
+                    for i in range(len(outputs)):
+                        x1, y1, x2, y2, idx = outputs[i]
+                        w = x2 - x1
+                        h = y2 - y1
+                        # frame_id, tracking_id, left, top, w, h , conf, x, y, z
+                        s = f"{idx_frame+1},{idx},{x1},{y1},{w},{h},1,-1,-1,-1\n"
+                        f.write(s)
+
+
+
+            idx_frame += 1
+
+        print('Avg YOLO time (%.3fs), Sort time (%.3fs) per frame' % (sum(yolo_time) / len(yolo_time),
+                                                            sum(sort_time)/len(sort_time)))
+        t_end = time.time()
+        print('Total time (%.3fs), Total Frame: %d' % (t_end - t_start, idx_frame))
+
+    def image_track(self, im0):
+        """
+        :param im0: original image, BGR format
+        :return:
+        """
+        # preprocess ************************************************************
+        # Padded resize
+        img = letterbox(im0, new_shape=self.img_size)[0]
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        # numpy to tensor
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        s = '%gx%g ' % img.shape[2:]    # print string
+
+        # Detection time *********************************************************
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():
+            pred = self.detector(img, augment=self.args.augment)[0]  # list: bz * [ (#obj, 6)]
+
+        # Apply NMS and filter object other than person (cls:0)
+        pred = non_max_suppression(pred, self.args.conf_thres, self.args.iou_thres,
+                                   classes=self.args.classes, agnostic=self.args.agnostic_nms)
+        t2 = time_synchronized()
+
+        # get all obj ************************************************************
+        det = pred[0]  # for video, bz is 1
+        if det is not None and len(det):  # det: (#obj, 6)  x1 y1 x2 y2 conf cls
+
+            # Rescale boxes from img_size to original im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Print results. statistics of number of each obj
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += '%g %ss, ' % (n, self.names[int(c)])  # add to string
+
+            bbox_xywh = xyxy2xywh(det[:, :4]).cpu()
+            confs = det[:, 4:5].cpu()
+
+            # ****************************** deepsort ****************************
+            outputs = self.deepsort.update(bbox_xywh, confs, im0)
+            # (#ID, 5) x1,y1,x2,y2,track_ID
+        else:
+            outputs = torch.zeros((0, 5))
+
+        t3 = time.time()
+        return outputs, t2-t1, t3-t2
+
+
+    def __read_img(self, path):
+        
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
+
+    def __gather_sequence_info(self, sequence_dir):
+        """Gather sequence information, such as image filenames, detections,
+        groundtruth (if available).
+
+        Parameters
+        ----------
+        sequence_dir : str
+            Path to the MOTChallenge sequence directory.
+        detection_file : str
+            Path to the detection file.
+
+        Returns
+        -------
+        Dict
+            A dictionary of the following sequence information:
+
+            * sequence_name: Name of the sequence
+            * image_filenames: A dictionary that maps frame indices to image
+            filenames.
+            * detections: A numpy array of detections in MOTChallenge format.
+            * groundtruth: A numpy array of ground truth in MOTChallenge format.
+            * image_size: Image size (height, width).
+            * min_frame_idx: Index of the first frame.
+            * max_frame_idx: Index of the last frame.
+
+        """
+        image_dir = os.path.join(sequence_dir, "img1")
+        image_filenames = {
+            int(os.path.splitext(f)[0]): os.path.join(image_dir, f) for f in os.listdir(image_dir)}
+        groundtruth_file = os.path.join(sequence_dir, "gt/gt.txt")
+
+        groundtruth = None
+        if os.path.exists(groundtruth_file):
+            groundtruth = np.loadtxt(groundtruth_file, delimiter=',')
+
+        if len(image_filenames) > 0:
+            image = cv2.imread(next(iter(image_filenames.values())),
+                            cv2.IMREAD_GRAYSCALE)
+            image_size = image.shape
+        else:
+            image_size = None
+            print(f"ERROR !!!! no imgs found in {sequence_dir}")
+
+        if len(image_filenames) > 0:
+            min_frame_idx = min(image_filenames.keys())
+            max_frame_idx = max(image_filenames.keys())
+        else:
+            min_frame_idx = 0
+            max_frame_idx = 0
+
+        info_filename = os.path.join(sequence_dir, "seqinfo.ini")
+        if os.path.exists(info_filename):
+            with open(info_filename, "r") as f:
+                line_splits = [l.split('=') for l in f.read().splitlines()[1:]]
+                info_dict = dict(
+                    s for s in line_splits if isinstance(s, list) and len(s) == 2)
+
+            update_ms = 1000 / int(info_dict["frameRate"])
+            # width = int(info_dict["imWidth"])
+            # height = int(info_dict["imHeight"])
+        else:
+            update_ms = None
+
+        seq_info = {
+            "sequence_name": os.path.basename(sequence_dir),
+            "image_filenames": image_filenames,
+            "groundtruth": groundtruth,
+            "image_size": image_size,
+            "min_frame_idx": min_frame_idx,
+            "max_frame_idx": max_frame_idx,
+            "update_ms": update_ms,
+            "frameRate": int(info_dict["frameRate"])
+            # "width": width,
+            # "height": height
+        }
+        return seq_info
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # input and output
-    parser.add_argument('--input_path', type=str, default='input_480.mp4', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--input_video', type=str, default='input_480.mp4', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--save_path', type=str, default='output/', help='output folder')  # output folder
     parser.add_argument("--frame_interval", type=int, default=2)
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--save_txt', default='output/predict/', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
 
+    # MOT data
+    parser.add_argument('--eval', action='store_true', default=False)
+    parser.add_argument('--seq_folder', default="/MOT16/test/MOT16-01/", help='folder contains all frame images, seq info, ...')
+    parser.add_argument('--save_result', default= "output/", help='Folder contains tracking results in .txt file as a MOT16 annotator')
     # camera only
     parser.add_argument("--display", action="store_true")
     parser.add_argument("--display_width", type=int, default=800)
@@ -259,7 +527,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.img_size = check_img_size(args.img_size)
     print(args)
-
-    with VideoTracker(args) as vdo_trk:
-        vdo_trk.run()
+    if args.eval:
+        with ImageTracker(args) as img_trk:
+            img_trk.run()
+    else:
+        with VideoTracker(args) as vdo_trk:
+            vdo_trk.run()
 
